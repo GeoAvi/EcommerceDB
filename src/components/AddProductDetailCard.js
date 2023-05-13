c94158613ce6336f9b525179b24bd9a1ab6f48f5
GeoAvi <122094972+GeoAvi@users.noreply.github.com>
2023-05-13 23:17:51 +0530

    Add files via upload

---
+++ src/components/AddProductDetailCard.js
@@ -0,0 +1,155 @@
+import { Add, Delete, Edit, Remove } from "@mui/icons-material";
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  IconButton,
+  Rating,
+  Typography,
+} from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  MENULIST,
+  addToCart,
+  deleteProduct,
+  editProduct,
+  removeFromCart,
+  updateMenu,
+} from "../redux/mainSlice";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect, useState } from "react";
+
+const ProductDetailsCard = ({ prod }) => {
+  const dispatch = useDispatch();
+  const { cart } = useSelector((state) => state.ecommercesite);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const filterCart = cart.filter((x) => x.item.id === prod.id);
+    if (filterCart && filterCart.length > 0) {
+      setCount(filterCart[0].count);
+    }
+  }, []);
+
+  const handleDeleteProduct = () => {
+    dispatch(deleteProduct(prod));
+    toast.error("Product has been deleted", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
+  const handleEditProduct = () => {
+    dispatch(editProduct(prod));
+    dispatch(updateMenu(MENULIST.AddProducts));
+  };
+
+  const handleAddToCartIncrement = () => {
+    setCount((count) => count + 1);
+    dispatch(
+      addToCart({
+        count: count + 1,
+        item: prod,
+      })
+    );
+  };
+
+  const handleAddToCartDecrement = () => {
+    setCount((count) => count - 1);
+    dispatch(
+      removeFromCart({
+        count: count - 1,
+        item: prod,
+      })
+    );
+  };
+
+  return (
+    <>
+      {/* <ToastContainer> */}
+      <Card sx={{ mb: 2 }}>
+        <CardContent sx={{ p: 4 }}>
+          <Grid container spacing={3}>
+            <Grid item md={2} sm={4} xs={12}>
+              <img src={prod.image} width="100%" alt={prod.name} />
+            </Grid>
+            <Grid item md={2} sm={2} xs={12}>
+              <Typography variant="h4" sx={{ fontWeight: "bold" }}>
+                {prod.title}
+              </Typography>
+              <Typography variant="h5" sx={{ mb: 5 }}>
+                Rs. {prod.price}
+              </Typography>
+
+              <Rating name="read-only" value={prod.rating} readOnly />
+            </Grid>
+            <Grid item md={8} sm={6} xs={12}>
+              <Typography variant="strong" sx={{ textAlign: "justify" }}>
+                {prod.description}
+              </Typography>
+              <br />
+              <Typography variant="div" component="div" sx={{ mt: 3 }}>
+                <Button
+                  size="small"
+                  variant="contained"
+                  sx={{ minWidth: "auto", p: 1 }}
+                  disabled={count === 0}
+                  onClick={handleAddToCartDecrement}
+                >
+                  <Remove fontSize="15px" htmlColor="white" />
+                </Button>
+                <Button
+                  size="small"
+                  variant="text"
+                  sx={{
+                    minWidth: "auto",
+                    p: 1,
+                    mx: 1,
+                    cursor: "default",
+                    fontSize: "1.4rem",
+                    lineHeight: "0",
+                  }}
+                >
+                  {count}
+                </Button>
+                <Button
+                  size="small"
+                  variant="contained"
+                  sx={{ minWidth: "auto", p: 1 }}
+                  onClick={handleAddToCartIncrement}
+                >
+                  <Add fontSize="15px" htmlColor="white" />
+                </Button>
+                <Typography>Quantity in Cart</Typography>
+              </Typography>
+              <Typography
+                variant="div"
+                component="div"
+                sx={{ mb: 2, textAlign: "right" }}
+              >
+                <IconButton onClick={handleEditProduct}>
+                  <Edit htmlColor="orange" />
+                </IconButton>
+
+                <IconButton sx={{ mr: 3, ml: 1 }} onClick={handleDeleteProduct}>
+                  <Delete htmlColor="red" />
+                </IconButton>
+                <ToastContainer />
+              </Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+      {/* </ToastContainer> */}
+    </>
+  );
+};
+
+export default ProductDetailsCard;
---
+++ src/redux/mainSlice.js
@@ -0,0 +1,128 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const MENULIST = {
+  ProductsList: "ProductsList",
+  AddProducts: "AddProducts",
+  ShowCart: "ShowCart",
+};
+
+export const mainSlice = createSlice({
+  name: "ecommercesite",
+  initialState: {
+    pageURL: MENULIST.ProductsList,
+    allProducts: [],
+    isEdit: null,
+    cart: [],
+  },
+  reducers: {
+    addAllProducts: (state, action) => {
+      state.allProducts = action.payload;
+    },
+    addNewProduct: (state, action) => {
+      let tmpAllProds = JSON.parse(JSON.stringify(state.allProducts)); //DEEP COPY
+      tmpAllProds = [...tmpAllProds, action.payload];
+      console.log(tmpAllProds);
+      state.allProducts = tmpAllProds;
+      tmpAllProds = null;
+      // state.pageURL = MENULIST.ProductsList;
+    },
+    updateMenu: (state, action) => {
+      state.pageURL = action.payload;
+    },
+    deleteProduct: (state, action) => {
+      state.allProducts = state.allProducts.filter(
+        (x) => x.id !== action.payload.id
+      );
+    },
+    sortProductByPrice: (state, action) => {
+      if (action.payload === "asc") {
+        state.allProducts.sort((a, b) => {
+          return a.price - b.price;
+        });
+      } else {
+        state.allProducts.sort((a, b) => {
+          return b.price - a.price;
+        });
+      }
+    },
+    editProduct: (state, action) => {
+      state.isEdit = action.payload;
+    },
+    updateProduct: (state, action) => {
+      let tmpAllProds = JSON.parse(JSON.stringify(state.allProducts)); // DEEP COPY
+      tmpAllProds = tmpAllProds.map((x) => {
+        if (x.id === action.payload.id) {
+          x = action.payload;
+        }
+        return x;
+      });
+      state.allProducts = tmpAllProds;
+      state.isEdit = null;
+
+      // state.pageURL = MENULIST.ProductsList;
+    },
+    addToCart: (state, action) => {
+      let tmpCart = JSON.parse(JSON.stringify(state.cart));
+      let flag = false;
+      tmpCart = tmpCart.map((x) => {
+        if (x.item.id === action.payload.item.id) {
+          x.count = action.payload.count;
+          flag = true;
+        }
+        return x;
+      });
+      if (flag === false) {
+        tmpCart = [
+          ...tmpCart,
+          {
+            count: action.payload.count,
+            item: action.payload.item,
+          },
+        ];
+      }
+      state.cart = tmpCart;
+      localStorage.setItem("cart", JSON.stringify(state.cart));
+    },
+    removeFromCart: (state, action) => {
+      let tmpCart = JSON.parse(JSON.stringify(state.cart));
+      let index = -1;
+      tmpCart.map((x, inx) => {
+        if (x.item.id === action.payload.item.id) {
+          index = inx;
+        }
+        return x;
+      });
+      if (index !== -1) {
+        if (tmpCart[index].count === 1) {
+          // Remove node from Cart
+          tmpCart.splice(index, 1);
+        } else {
+          tmpCart[index].count -= 1;
+        }
+      }
+      state.cart = tmpCart;
+      localStorage.setItem("cart", JSON.stringify(state.cart));
+    },
+    resetCartFromStorage: (state) => {
+      state.cart = JSON.parse(localStorage.getItem("cart")) || [];
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const {
+  addAllProducts,
+  updateMenu,
+  deleteProduct,
+  sortProductByPrice,
+  addNewProduct,
+  editProduct,
+  updateProduct,
+  addToCart,
+  removeFromCart,
+  resetCartFromStorage,
+} = mainSlice.actions;
+
+export default mainSlice.reducer;
